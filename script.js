65ca35ba5252678b6d08df6c03ebee28643ceaec
ericdavidanderson <burrej1@gmail.com>
2021-08-24 15:43:08 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var Q1= "What is the name of the HTML element that we put JavaScript in?";
+var Q2= "Where is the correct place to insert a JavaScript?";
+var Q3= "What is the correct syntax for referring to an external script called 'xxx.js'?";
+var Q4= "What is the correct way to write 'message' in an alert box?";
+var Q5= "How do you create a function in JavaScript?";
+var Q6= "How do you call a function named xFunction?";
+
+
+
+
+alert("Are you ready to test your JavaScript skills?");
+
